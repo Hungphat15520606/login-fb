170ce70b8e1f39d1134d5c428629d832f674ad44
Hung Phat <phatth2016@gmail.com>
2019-04-24 19:32:56 +0700

    .

---
+++ build/precache-manifest.7e4061907dd12042c10c9960d47a870a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a9a594b99fb5e5545519",
+    "url": "/static/js/main.cef4518a.chunk.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "a9a594b99fb5e5545519",
+    "url": "/static/css/main.a0c8252f.chunk.css"
+  },
+  {
+    "revision": "16ae3adf9a4b3b74bec51edf3d8dbd45",
+    "url": "/index.html"
+  }
+];
